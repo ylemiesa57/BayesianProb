19ff1d1ab91266f75a7bd6c626ee84c93a269ff5
Yaphet Kumsa Lemiesa <yaphet.lemiesa@gmail.com>
2024-07-09 15:44:02 -0400

    fixed bugs with joint prob

--- BN.py
+++ BN.py
@@ -26,26 +26,37 @@ class BayesianNetwork:
                 parents.append(parent)
         return parents
 
-    #set the kwargs to what you want each variable to be in terms of state. bascilly testing prob of certain conditions being true
-    def joint_probability(self, **kwargs):
-        prob = 1.0
-        for node, value in kwargs.items():
-            parents = self.get_parents(node)
-
-            #if there is no parents for node
-            if not parents:
-                prob *= self.cpts[node][value]
-
-            #if it is a child node, and has parents
-            else:
-                parent_values = tuple(kwargs[parent] for parent in parents)
-                # add handling here to compare if the parent is only 1, which if is the case
-                if len(parents) == 1:
-                    prob *= self.cpts[node][parent_values[0]][value]
-                else:
-                    prob *= self.cpts[node][parent_values][value]
-                prob *= self.cpts[node][parent_values][value]
-        return prob
+    # #set the kwargs to what you want each variable to be in terms of state. bascilly testing prob of certain conditions being true
+    # def joint_probability(self, **kwargs):
+    #     '''
+    #     pass in all nodes in kwargs to do inference
+    #     '''
+    #     prob = 0
+    #     # Get all nodes not in kwargs
+    #     missing_nodes = [node for node in self.nodes if node not in kwargs]
+        
+    #     # Generate all possible combinations for missing nodes, IF THERE ARE MISSING NODES. SHOULDN'T BE!!!!!
+    #     for combination in itertools.product(*[self.nodes[node] for node in missing_nodes]):
+    #         full_assignment = kwargs.copy()
+    #         full_assignment.update(dict(zip(missing_nodes, combination)))
+            
+    #         temp_prob = 1
+    #         for node, value in full_assignment.items():
+    #             parents = self.get_parents(node)
+    #             # if no parents
+    #             if not parents:
+    #                 temp_prob *= self.cpts[node][value]
+    #             # if it has parents
+    #             else:
+    #                 parent_values = tuple(full_assignment[parent] for parent in parents)
+    #                 if len(parents) == 1:
+    #                     temp_prob *= self.cpts[node][parent_values[0]][value]
+    #                 else:
+    #                     temp_prob *= self.cpts[node][parent_values][value]
+            
+    #         prob += temp_prob
+        
+    #     return prob
     
 
     def joint_probability(self, **kwargs):
@@ -147,6 +158,9 @@ bn.set_cpt('Guest', guest_cpt)
 bn.set_cpt('Prize', prize_cpt)
 bn.set_cpt('Monty', monty_cpt)
 
+
+print("Monty Hall Tests!")
+
 # Example of computing a joint probability
 jp = bn.joint_probability(Guest='A', Prize='B', Monty='C')
 print(f'Joint Probability P(Guest=A, Prize=B, Monty=C): {jp}')
@@ -160,6 +174,7 @@ print(inf)
 inf1 = bn.inference(target_node='Prize', Guest='A', Monty='C')
 print(inf1)
 
+print("############################")
 
 
 cyberbn = BayesianNetwork()
@@ -227,25 +242,10 @@ cyberbn.set_cpt('ssh(2)', serv_ssh_2_cpt)
 cyberbn.set_cpt('<ssh, 1, 2>', vul_ssh_1_2_cpt)
 cyberbn.set_cpt('user(2)', priv_user_2_cpt)
 
+print("Attack graph tests")
+
 inf = cyberbn.inference(target_node='<ssh, 1, 2>')
-print(inf)
+print("Inference on <ssh, 1, 2>. Return prob dist: ", inf)
 
-
-
-
-'''
-Traceback (most recent call last):
-  File "/Users/yaphetlemiesa/Documents/Summer Urop/Bayesian Probabilities with Pomegranate.py", line 276, in <module>
-    inf = cyberbn.inference(target_node='<ssh, 1, 2>')
-          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/Users/yaphetlemiesa/Documents/Summer Urop/Bayesian Probabilities with Pomegranate.py", line 153, in inference
-    joint_prob = self.joint_probability(**full_assignment)
-                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/Users/yaphetlemiesa/Documents/Summer Urop/Bayesian Probabilities with Pomegranate.py", line 116, in joint_probability
-    prob *= self.cpts[node][parent_values][value]
-            ~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^
-KeyError: ('T',)
-'''
-
-
+print("###########")
 
--- README.md
+++ README.md
@@ -1,5 +1,65 @@
-Project working with Dr. Ranjan Pal
+#Project working with Dr. Ranjan Pal
 
 Developing bayesian network algorithm for inference and simulating cyber networks with a host privilege based attack graph
 
-future: AI applications
+##future: AI applications
+
+
+###Here's how we access it:
+
+self.cpts[node] gives us the entire CPT dictionary for 'user(2)'
+parent_values[0] gives us the state of the parent node ('<ssh, 1, 2>')
+value is the state of 'user(2)' we're querying
+
+So, self.cpts[node][parent_values[0]][value] might look like:
+self.cpts['user(2)']['T']['T'], which would give us 1.
+
+------------------------------
+
+self.cpts[node] gives us the entire CPT dictionary for '<ssh, 1, 2>'
+parent_values is a tuple of the states of all parent nodes, in order
+value is the state of '<ssh, 1, 2>' we're querying
+
+So, self.cpts[node][parent_values][value] might look like:
+self.cpts['<ssh, 1, 2>'][('T', 'T', 'T')]['T'], which would give us 0.08.
+
+--------------------------------
+Let's say we have a network A -> B -> C -> D. What if we just asked for the joint probability P(B=?,C=?,D=?)
+* We would have to compute using marginalization and conditional probabilities. Ex. below
+P(B,C,D) = ∑[A] P(A,B,C,D) = ∑[A] P(D|C) * P(C|B) * P(B|A) * P(A)
+
+* This is implemented in the Joint Probability function where we generate all possible combinations for missing nodes
+if there all the nodes in the "kwargs" passed in aren't all the kwargs in the bayesian network.
+
+
+
+Output:
+
+Joint Probability: A single probability value.
+Inference: A probability distribution for the target variable.
+
+Key differences:
+
+Output:
+
+Joint Probability: A single probability value. (When values are unspecificed, it means we sum over the the prbobabilities of every state they are in.
+this is the marginalize, NOT INTENDED PURPOSE THOUGH!!!)
+Inference: A probability distribution for the target variable.
+
+
+Marginalization:
+
+Joint Probability: Doesn't try to marginalize over unspecified variables. Joint probability is for chance of certain conditions being met. If there is other variables unspecificed, then we just add up the probabilities for all their possibilities BUT NOT INDENDED INPUT!!!)
+Inference: Marginalizes over all unspecified variables always.
+
+
+Flexibility:
+
+Joint Probability: Requires specifying values for all variables of interest.
+Inference: Can handle partial information (evidence) and unknown variables.
+
+
+Use case:
+
+Joint Probability: Used when you want the probability of a specific scenario.
+Inference: Used when you want to predict the probability distribution of a variable given some evidence.
